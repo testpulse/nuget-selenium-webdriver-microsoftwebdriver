492a1ab2148a972d2f8a9b933c41505c91743ddf
Raffles Gun <rafflesgun@gmail.com>
2017-03-24 23:44:22 +0800

    Add TestDrive project to test publishing driver file when "_PUBLISH_MSWEBDRIVER" compilation symbol is defined.

--- BuildPackage.bat
+++ BuildPackage.bat
@@ -1,11 +1,5 @@
 @echo off
 pushd %~dp0
 
-REM Comment out for downloading driver file for time being
-REM echo Downloading %fname%...
-REM powershell -noprof -exec unrestricted -c ".\download-driver.ps1"
-REM echo.
-:SKIP_DOWNLOAD
-
 echo Packaging...
-.\NuGet.exe pack .\Selenium.WebDriver.MicrosoftWebDriver.nuspec
+.\buildTools\NuGet.exe pack .\src\Selenium.WebDriver.MicrosoftWebDriver.nuspec -Out .\dist
